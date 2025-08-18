f02102305a378aa382991c1edd2f84e69f228bde
SamanthaPatricio <sammy.ivel@gmail.com>
2025-08-17 21:47:40 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";           // <- ESTE NOME PRECISA EXISTIR EM src/index.css
+import "./index.css"; // <- ESTE ARQUIVO PRECISA EXISTIR EM src/index.css
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

af15a4c1ae5e3f9e59e77cec6b19e21c12acbc39
SamanthaPatricio <sammy.ivel@gmail.com>
2025-08-17 21:19:32 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#9d7c2b", // dourado do seu logo
+          50: "#fff8e6",
+          100: "#f9edc2",
+          200: "#efd88a",
+          300: "#e2bf54",
+          400: "#caa03a",
+          500: "#9d7c2b",
+          600: "#775f21",
+          700: "#544416",
+          800: "#3a2f0f",
+          900: "#231c08",
+        },
+      },
+    },
   },
   plugins: [],
 }

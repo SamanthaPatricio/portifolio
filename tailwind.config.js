38f88ab117d4d50a4e9c0b67477d2181f9c16002
SamanthaPatricio <sammy.ivel@gmail.com>
2025-08-19 20:48:58 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         brand: {
-          50:  "#fff7e6",
-          100: "#fdecc8",
-          200: "#f9d895",
-          300: "#f3c162",
-          400: "#e9a93b",
-          500: "#d9961e",
-          600: "#b97917",
-          700: "#9d7c2b",   // seu dourado
-          800: "#6b4b0f",
-          900: "#4a3207",
+          50: "#FFF8E6",
+          700: "#9d7c2b",
         },
       },
-      boxShadow: { soft: "0 8px 30px rgba(0,0,0,.06)" },
+      boxShadow: {
+        soft: "0 8px 22px rgba(0,0,0,.08)",
+      },
     },
   },
   plugins: [],

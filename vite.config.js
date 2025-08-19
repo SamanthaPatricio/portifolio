3f71a3c15bfe16f26610b2dbc046260e2da4b1ec
SamanthaPatricio <sammy.ivel@gmail.com>
2025-08-19 19:00:12 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",          // caminhos absolutos, evita 404
-  build: {
-    outDir: "dist",   // pasta que a Vercel vai servir
-    assetsDir: "assets",
-    sourcemap: false,
-  },
+  base: "/",
+  build: { outDir: "dist", assetsDir: "assets", sourcemap: false },
   server: { host: true, port: 5173 }
 });

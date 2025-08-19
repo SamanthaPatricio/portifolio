50091a04e8efa75c0bb5b24b1ab484ad2e0ec80b
SamanthaPatricio <sammy.ivel@gmail.com>
2025-08-19 19:47:56 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",                 // importante para Vercel
-  build: { outDir: "dist" }  // Vercel vai servir /dist
 });
